7e0c4b8c168234dc6ffd821aa8a0e0c7f048e2fa
Rafael Evald <evald.lich@gmail.com>
2022-09-17 15:53:41 -0400

    Scripts do banco de dados

---
+++ documentacao/dbBancoDeDados.sql
@@ -0,0 +1,19 @@
+CREATE TABLE perfils_usuario(
+    codigo integer, 
+    descricao varchar(50),
+
+    CONSTRAINT pk_perfils_usuario PRIMARY KEY (codigo)
+);
+
+CREATE TABLE usuarios (
+    codigo integer,
+    data_de_cadastro date,
+    usuario char(11),
+    senha varchar(50),
+    nome_completo varchar(50),
+    status_usuario varchar(20),
+    fk_perfil integer,
+
+    CONSTRAINT pk_usuarios PRIMARY KEY (codigo),
+    FOREIGN KEY (fk_perfil) REFERENCES perfils_usuario(codigo)
+);
